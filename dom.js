6ddbac1552bc45484390b5ed9c0b931818137513
Guilherme Ribeiro <guilherme@martizi.com>
2019-10-12 13:53:02 -0300

    fix move actions and attack actions

--- dom.js
+++ dom.js
@@ -1,30 +1,74 @@
+// // intro game
+
+// let startButton = document.querySelector('button')
+// startButton.onclick = function(e){
+//     let homeIntro = document.getElementsByClassName('home')
+//     console.log(homeIntro[0])
+//     homeIntro[0].style.visibility = 'hidden'
+// }
+
+// // 
+
 let game = new Game();
 let battlefield = game.startGame()
 
-let playerOnePhase = battlefield.playerOnePhase
-let playerTwoPhase = battlefield.playerTwoPhase
+let playerOnePhase = [...battlefield.playerOnePhase]
+let playerTwoPhase = [...battlefield.playerTwoPhase]
 battlefield = battlefield.battlefied
 
 let board = document.querySelector('#board')
 let html = ''
-let result = ''
+// let oldPosition = ''
+let oldChild
 
 
-function updateRender(position) {
+function updateRender(position, oldPosition) {
     if(position == undefined){
         position = false
+    }
+    if(oldPosition == undefined){
+        oldPosition = false
+    }
+    if(char == undefined){
+        char = false
     }
     console.log(position)
     battlefield.forEach((line, indexLine) => {
         line.forEach((square, indexSquare) => {
             let className = 'square'
+            //verificar se é uma square normal e colo
+            if(square == 0){
+                document.getElementById(`${indexLine}-${indexSquare}`).classList = className
+            }
+            //verifica se tem x de range e pinta de azul com move
             if(square == 'x') {
                 document.getElementById(`${indexLine}-${indexSquare}`).classList.value += ' move'
             }
-            // if(position == `${indexLine}${indexSquare}`){
-            //     document.getElementById(`${indexLine}-${indexSquare}`).children[0].style.visibility = 'visible'
-            //     document.getElementById(`${indexLine}-${indexSquare}`).children[1].style.visibility = 'visible'
-            // }
+            if(square == 'o') {
+                document.getElementById(`${indexLine}-${indexSquare}`).classList.value += ' attack'
+            }
+            //verifica se tinha personagem e apaga imagem
+            if(oldPosition){
+                let element = document.getElementById(`${oldPosition[0]}-${oldPosition[1]}`)
+                oldChild = document.getElementById(`${oldPosition[0]}-${oldPosition[1]}`).children[2]
+                element.removeChild(oldChild)
+                oldPosition = undefined
+            }
+            //verifica se tem personagem e coloca imagem
+            if(square && position && actType == 'move') {
+                let element = document.getElementById(`${position}`)
+                element.classList.value = 'square'
+                element.appendChild(oldChild)
+
+            }
+            if(game.actions.move == 1 && game.actions.attack == 0 && position ){
+                document.getElementById(`${position}`).children[1].style.visibility = 'visible'
+                document.getElementById(`${position}`).children[0].style.visibility = 'hidden'
+            }
+            if(game.actions.move == 0 && game.actions.attack == 1 && position ){
+                document.getElementById(`${position}`).children[0].style.visibility = 'visible'
+                document.getElementById(`${position}`).children[1].style.visibility = 'hidden'
+            }
         });
     });
 }
@@ -35,16 +79,25 @@ function render(){
         line.forEach((square, indexSquare) => {
             let className = 'square'
             if(square) {
-                className = 'square hascharacter'
+                html += `<div id="${indexLine}-${indexSquare}" class="${className}" data-js-position-line="${indexLine}" data-js-position-square="${indexSquare}">
+                <div class="option-move">
+                    <p>move</p>
+                </div>
+                <div class="option-attack">
+                    <p>attack</p>   
+                </div>
+                <img class="image-character" src="./images/class-persons-images-sprites/${battlefield[indexLine][indexSquare].image[0]}">
+                </div>`
+            } else {
+                html += `<div id="${indexLine}-${indexSquare}" class="${className}" data-js-position-line="${indexLine}" data-js-position-square="${indexSquare}">
+                <div class="option-move">
+                    <p>move</p>
+                </div>
+                <div class="option-attack">
+                    <p>attack</p>
+                </div>
+                </div>`
             }
-            html += `<div id="${indexLine}-${indexSquare}" class="${className}" data-js-position-line="${indexLine}" data-js-position-square="${indexSquare}">
-            <div class="option-move">
-                <p>move</p>
-            </div>
-            <div class="option-attack">
-                <p>attack</p>
-            </div>
-            </div>`
         });
         html += `</div>`
     });
@@ -53,93 +106,152 @@ function render(){
 render()
 board.innerHTML = html
 
-
+let char
+let indexChar
 function phase(){
+    if(game.endTurnFlag == 'start'){
+        playerTurn = [...playerOnePhase]
+    }
     
-    playerTurn = playerOnePhase
-
-    //pega um personagem aleatorio pra começar
-    let indexChar = Math.floor(Math.random() * playerOnePhase.length)
-
-    //instancia o objeto do personagem
-    let char = playerTurn[indexChar]
-
-    //Habilita a visualização das duas opções de ação(move/attack) do personagem instanciado
-    document.getElementById(`${char.position[0]}-${char.position[1]}`).children[0].style.visibility = 'visible'
-    document.getElementById(`${char.position[0]}-${char.position[1]}`).children[1].style.visibility = 'visible'
-
-    //instancia os dois elementos acima
-    moveAct = document.getElementById(`${char.position[0]}-${char.position[1]}`).children[0]
-    attackAct = document.getElementById(`${char.position[0]}-${char.position[1]}`).children[1]
-
-    //Habilita div de movimento para se tornar clicável e disparar o método range() e desenhar na tela o range de movimento a partir do 
-    // metodo updateRender() 
-    moveAct.onclick = function(event){
-        positionArgument = `${char.position[0]}${char.position[1]}`
-        char.range(char.move,battlefield)
-        updateRender()
+    if(game.actions.move == 1 && game.actions.attack == 1){
+        game.actions.move = 0
+        game.actions.attack = 0
+        if(playerTurn.length === 0) {
+            game.turn += 1
+            if(game.turn%2 === 0){
+                playerTurn = [...playerTwoPhase]
+            } else {
+            playerTurn = [...playerOnePhase]
+            }
         }
+    }
 
-    //!!!!!!  DESAFIO !!!!!!!//
-    //  feito os passos acimas nós conseguimos iniciar a jogada do personagem, acionar o movimento e desenhar o range na tela, agora falta selecionar
-    // o quadrado desejado para deslocar o personagem.
-    //  Qual foi a maneira que desenhei pra realizar isso:
-    // 1 - para mover o personagem eu preciso pegar a nova posição na matriz que eu clicar, oq eu fiz foi criar uma função rangeTarget() que basicamente faz:
-    // um for por todas as divs, squares, 'espaços de movimento na matriz' passando evento para todas, e assim que clicada, retornar o id daquela div que
-    // é a posição da matriz, entretanto, nos meus testes não consegui interagir com o retorno dela, então, eu criei um atributo na minha classe
-    // Game para poder armazenar essa posição e posteriormente pega la e joga la como argumento na minha função moveAct, função essa responsável
-    // para mudar o personagem na matriz para a nova posição.
+    if(game.actions.move == 0 && game.actions.attack == 0){
+        typeAct = ''
 
-    rangeTarget()
-
-    console.log(result)
-
-    //!!!!!! aqui o codigo deveria esperar eu clicar para pegar a posição indicada para depois continuar e não sei como faz isso e se está certo meu
-    //raciocinio
-
-    targetClickSquare =  document.getElementById(game.moves[2]) // <---quando chega aqui o codigo, não existe o game.moves ainda, pois só se criar
-    // depois que clicar, ou seja, o codigo ta rodando a função toda e eu não sei como criar um 'espaço de espera' para poder pegar a posição nova.
+        //pega um personagem aleatorio pra começar
+        indexChar = Math.floor(Math.random() * playerTurn.length)
     
-    //aqui seria o momento que ao clicar no quadrado novo ele executaria 
-    targetClickSquare.onclick = function(event){
-        moveAct(char.position, game.moves[1], battlefield)
-        updateRender(positionArgument)
-        }
-    //tira o personagem do turno
-    playerTurn.splice(playerTurn.indexOf(playerTurn[indexChar]),1)
-    
+        //instancia o objeto do personagem
+        char = playerTurn[indexChar]
+
+        //Habilita a visualização das duas opções de ação(move/attack) do personagem instanciado
+        document.getElementById(`${char.position[0]}-${char.position[1]}`).children[0].style.visibility = 'visible'
+        document.getElementById(`${char.position[0]}-${char.position[1]}`).children[1].style.visibility = 'visible'
+
+        // //instancia os dois elementos acima
+        // let moveAct = document.getElementById(`${char.position[0]}-${char.position[1]}`).children[0]
+        // let attackAct = document.getElementById(`${char.position[0]}-${char.position[1]}`).children[1]
+
+        clickableActions()
+    }
+
+    if(game.actions.move == 1 && game.actions.attack == 0) {
+        clickableActions()
+    }
+
+    if(game.actions.attack == 1 && game.actions.move == 0){
+        clickableActions()
+    }
+
+    if(game.actions.attack == 1 && game.actions.move == 1){
+        game.actions.attack =  0
+        game.actions.move = 0
+        phase()
+    }
 }
 
-function rangeTarget(){
+
+function rangeTarget(actType){
     let squaresDivs = document.querySelectorAll('.square')
     let linePosition
     let positionArgument
     squaresDivs.forEach(square => {
         square.onclick = function(event) {
-            linePosition = event.target.attributes['data-js-position-line'].value
-            squarePosition = event.target.attributes['data-js-position-square'].value
-            positionArgument = [linePosition,squarePosition]
-            result = {1:positionArgument, 2:`${linePosition}-${squarePosition}`}
-            return result
+            event.stopPropagation()
+            console.log(actType)
+            // linePosition = event.target.attributes['data-js-position-line'].value
+            // squarePosition = event.target.attributes['data-js-position-square'].value
+            // positionArgument = [linePosition,squarePosition]
+            console.log('novo',square.id)
+            let idSquare = square.id.split('-')
+            let positionArgument = [parseFloat(square.id.split('-')[0]),parseFloat(square.id.split('-')[1]) ]
+            game.moves =  {1:positionArgument, 2:`${square.id}`}
+            if(actType == 'move'){
+                game.actions.move += 1
+                document.getElementById(`${char.position[0]}-${char.position[1]}`).children[0].style.visibility = 'hidden'
+                document.getElementById(`${char.position[0]}-${char.position[1]}`).children[1].style.visibility = 'hidden'
+                let oldPosition = char.position
+                char.moveAct(char.position, game.moves[1], battlefield)
+                if(game.actions.move == 1 && game.actions.attack == 1){
+                    playerTurn.splice(playerTurn.indexOf(playerTurn[indexChar]),1)
+                    phase()
+                } else {
+                    updateRender(game.moves[2], oldPosition)
+                    phase()
+                }
+
+            }
+            if(actType == 'attack'){
+                game.actions.attack += 1
+                document.getElementById(`${char.position[0]}-${char.position[1]}`).children[1].style.visibility = 'hidden'
+                document.getElementById(`${char.position[0]}-${char.position[1]}`).children[0].style.visibility = 'hidden'
+                char.rangeAttack(battlefield)
+                // char.attack(idSquare, battlefield)
+                if(game.actions.move == 1 && game.actions.attack == 1){
+                    playerTurn.splice(playerTurn.indexOf(playerTurn[indexChar]),1)
+                    game.cleaner('o', battlefield)
+                    updateRender(game.moves[2])
+                    game.endTurnFlag = 'end'
+                    phase()
+                } else {
+                updateRender(game.moves[2])
+                game.endTurnFlag = 'end'
+                phase()
+                }
+            }
         }
     });
-}
     
+}
+
+let moveDivs = document.querySelectorAll('.option-move')
+let attackDivs = document.querySelectorAll('.option-attack')
+
+function clickableActions(){
+    moveDivs.forEach(moveActionDiv => {
+        moveActionDiv.onclick = function(event){
+            actType = 'move'
+            event.stopPropagation()
+            positionArgument = `${char.position[0]}${char.position[1]}`
+            char.range(char.move,battlefield)
+            updateRender()
+            rangeTarget(actType)
+        }
+        
+    });
+    attackDivs.forEach(attackActionDiv => {
+        attackActionDiv.onclick = function(event){
+            actType = 'attack'
+            event.stopPropagation()
+            positionArgument = `${char.position[0]}${char.position[1]}`
+            char.rangeAttack(battlefield)
+            updateRender()
+            rangeTarget(actType)
+            } 
+    })
+}
+
 phase()
 
-// for (let player of round){
-//     for (let character of player) {
-//         let position = character.position
-//         square = document.getElementById(`${position[0]}-${position[1]}`)
-//         square.onclick = function(event){
-//             linePosition = event.target.attributes['data-js-position-line'].value
-//             squarePosition = event.target.attributes['data-js-position-square'].value
-//             positionArgument = `${linePosition}${squarePosition}`
-//             if (battlefield[linePosition][squarePosition]){
-//                 let char = battlefield[linePosition][squarePosition]
-//                 char.range(char.move,battlefield)
-//                 updateRender(positionArgument)
-//             }    
-//         }       
-//     }
-// }
+
+
+
+
+
+
+
+// document.addEventListener("click",handler,true)
+
+
+
--- main.js
+++ main.js
@@ -35,16 +35,21 @@ class PersonJob {
         //must be a integer number '2'
         this.position = position;
         };
-    attack(opponent) {
-        console.log(opponent)
-
+    attack(opponent, matrix) {
+        opponent = matrix[opponent[0]][opponent[1]]
         let dano = Math.floor(Math.random()*(this.damage[0], this.damage[1]));
-        opponent.healthPoints -=  dano;
-        return dano    
+        opponent.healthPoints -=  dano;   
     }
 
     ckeckRange(){
         //to do
+    }
+    checkSquareIsAttackable(position, matrix){
+        if(matrix[position[0]][position[1]] != '' ){
+            return matrix[position[0]][position[1]]
+            }else{
+                return 'o'
+            } 
     }
 
     checkSquareIsWalkable(position, matrix){
@@ -55,11 +60,16 @@ class PersonJob {
         } 
     }
 
+
+
     moveAct(oldPosition,newPosition, matrix){
         console.log(oldPosition)
         console.log(newPosition)
         let char = matrix[oldPosition[0]][oldPosition[1]]
+        matrix[oldPosition[0]][oldPosition[1]] = 'x'
         matrix[newPosition[0]][newPosition[1]] = char
+        char.position = [newPosition[0],newPosition[1]]
+        game.cleaner('x', matrix)
     }
 
     vertRange(range, matrix) {
@@ -112,27 +122,83 @@ class PersonJob {
             } 
         }
     }
+
+    rangeAttack(matrix){
+        let position = this.position
+        let range = this.attackRange
+        matrix[position[0]][position[1]] = this.checkSquareIsWalkable(this.position, matrix)
+        //vertical line bellow position
+        for(let i = position[0] +1; i <= position[0] + range; i++){
+            if (matrix[i] == undefined) {
+                continue
+            } else {
+                matrix[i][position[1]] = this.checkSquareIsAttackable([i,position[1]], matrix)
+            }
+        }
+        //vertical line above position
+        for(let i = position[0] - range; i <= position[0] -1; i++){
+            if (matrix[i] == undefined) {
+                continue
+            } else {
+                matrix[i][position[1]] = this.checkSquareIsAttackable([i,position[1]], matrix)
+            }
+        }
+        //horizontal line before position
+        for(let i = position[1] - range; i <= position[1] -1; i++){
+            if (matrix[i] == undefined) {
+                continue
+            } else {
+                matrix[position[0]][i] = this.checkSquareIsAttackable([position[0],i], matrix)
+            }
+        }
+        // horizontal line after position
+        for(let i = position[1] + 1; i <= position[1] + range; i++){
+            if (matrix[i] == undefined) {
+                continue
+            } else {
+                matrix[position[0]][i] = this.checkSquareIsAttackable([position[0],i], matrix)
+            }
+        }
+}
 }
 
 class Game {
-    constructor(turn, phase, round, movesChars) {
-        this.turn = 0
-        this.round = 0
+    constructor(turn, phase, round, movesChars, actions, endTurnFlag ) {
+        this.turn = 1
+        this.phase = 0
         this.round = 0
         this.moves = {}
+        this.actions = {attack:0, move:0}
+        this.endTurnFlag = 'start'
+    }
+    cleaner(typeAct,matrix){
+        let cntLine=0
+        let cntSquare=0
+        for(let line of matrix){
+            for(let square of line){
+                if(square == typeAct){
+                    matrix[cntLine][cntSquare] = 0
+                    cntSquare +=1
+                } else {
+                    cntSquare +=1
+                }
+            }
+            cntSquare = 0
+            cntLine +=1
+        }
     }
     startGame() {
         //Characters obj creating
-        let squire = new PersonJob('Squire','This is for my team!',[6,11],1,3,43,null,null,null,4,[0,0])
-        let thief = new PersonJob('Thief','Do you think this corpse has some money?',[3,6],1,7,39,null,null, null,6,[0,0])
-        let whiteMage = new PersonJob('White Mage','May your God welcome you the way you deserve',[4,6],1,3,36,40,null,null,3,[0,0])
-        let blackMage = new PersonJob('Black Mage', '...hmm',[4,6],1,4,37,40,null,null,3,[0,0])
-        let lancer = new PersonJob('Lancer', 'Pierced by my spear!', [7,8],2,5,51,null,null,null,5,[0,0])
-        let knight = new PersonJob('Knight', 'One more to ennoble my honour!',[7,8],1,5,51,null,null,null,4,[0,0])
-        let bard = new PersonJob('Bard', 'Whistling song', [1,9],3,2,38,35,null,null,4,[0,0])
-        let archer = new PersonJob('Archer','Right on target!', [5,9],7,5,40,null,null,null,5,[0,0])
-        let monk = new PersonJob('Monk','I should meditate now \'oom\'',[3,15],1,6,30,null,null,null,5,[0,0])
-        let paladin = new PersonJob('Paladin','God Bless You.',[6,8],1,5,44,null,null,null,4,[0,0])
+        let squire = new PersonJob('Squire','This is for my team!',[6,11],1,3,43,null,null,['squire-1.png'],4,[0,0])
+        let thief = new PersonJob('Thief','Do you think this corpse has some money?',[3,6],1,7,39,null,null, ['thief-1.png'],6,[0,0])
+        let whiteMage = new PersonJob('White Mage','May your God welcome you the way you deserve',[4,6],1,3,36,40,null,['white-mage-1.png'],3,[0,0])
+        let blackMage = new PersonJob('Black Mage', '...hmm',[4,6],1,4,37,40,null,['black-mage-1.png'],3,[0,0])
+        let lancer = new PersonJob('Lancer', 'Pierced by my spear!', [7,8],2,5,51,null,null,['lancer-1.png'],5,[0,0])
+        let knight = new PersonJob('Knight', 'One more to ennoble my honour!',[7,8],1,5,51,null,null,['knight-1.png'],4,[0,0])
+        let bard = new PersonJob('Bard', 'Whistling song', [1,9],3,2,38,35,null,['bard-1.png'],4,[0,0])
+        let archer = new PersonJob('Archer','Right on target!', [5,9],7,5,40,null,null,['archer-1.png'],5,[0,0])
+        let monk = new PersonJob('Monk','I should meditate now \'oom\'',[3,15],1,6,30,null,null,['monk-1.png'],5,[0,0])
+        let paladin = new PersonJob('Paladin','God Bless You.',[6,8],1,5,44,null,null,['paladin-1.png'],4,[0,0])
 
         //array with all 10 characters
         let playerOne = [squire, thief, whiteMage, blackMage, lancer, knight, bard, archer, monk, paladin]
